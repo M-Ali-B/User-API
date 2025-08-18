b212b6e765127b6c3c162223c5e2947e29dda331
m-ali-b <alibabatorjan001@gmail.com>
2025-08-18 12:24:06 +0500

    created ecommerce schema

--- database/dbUtility.js
+++ database/dbUtility.js
@@ -37,7 +37,10 @@ export async function createMemberTable() {
                   id INTEGER PRIMARY KEY AUTOINCREMENT, 
                   username TEXT NOT NULL UNIQUE, 
                   password TEXT NOT NULL,
-                  role TEXT NOT NULL 
+                  role TEXT NOT NULL,
+                  phone TEXT,
+                  address TEXT,
+                  created_at DATETIME DEFAULT CURRENT_TIMESTAMP 
             )
       
       `)
@@ -56,12 +59,12 @@ export async function seedMemberTable() {
 
     await db.exec('BEGIN TRANSACTION')
 
-    for (const { username, password, role } of members) {
+    for (const { username, password, role, phone, address, created_at } of members) {
 
       await db.run(`
-        INSERT INTO members (username, password, role)
-        VALUES (?, ?, ?)`,
-        [username, password, role]
+        INSERT INTO members (username, password, role, phone,address,created_at)
+        VALUES (?, ?, ?,?, ?, ?)`,
+        [username, password, role, phone, address, created_at]
       )
 
     }
@@ -81,7 +84,7 @@ export async function seedMemberTable() {
 }
 
 export async function viewAllMembers() {
-const db = await open({
+  const db = await open({
     filename: path.join('database.db'),
     driver: sqlite3.Database
   });
@@ -122,41 +125,41 @@ export async function findMembersByCredientials(username, password) {
 }
 export async function insertMember(username, password, role) {
 
-const db  = await open({
-  filename : path.join('database.db'),
-  driver: sqlite3.Database
-})
+  const db = await open({
+    filename: path.join('database.db'),
+    driver: sqlite3.Database
+  })
 
-try {
-  await db.exec('BEGIN TRANSACTION');
+  try {
+    await db.exec('BEGIN TRANSACTION');
     await db.run(`
       INSERT INTO members (username,password,role)
       VALUES (?, ?, ?)`,
-      [username,password,role]
-      )
+      [username, password, role]
+    )
 
-      await db.exec('COMMIT');
-      return true; // 🔑 important: return true to indicate success
-}
+    await db.exec('COMMIT');
+    return true; // 🔑 important: return true to indicate success
+  }
 
-catch(err) { 
-await db.exec('ROLLBACK');
-if (err.code === 'SQLITE_CONSTRAINT') { 
+  catch (err) {
+    await db.exec('ROLLBACK');
+    if (err.code === 'SQLITE_CONSTRAINT') {
       console.error("❌ Username already exists:", username);
       return "DUPLICATE"; // 🔑 important: return a specific value for duplicate error
     } else {
       console.error("❌ DB Error:", err.message);
-    return false
+      return false
     }
-   // throw err; // 🔑 important: rethrow so API can respond with error
+    // throw err; // 🔑 important: rethrow so API can respond with error
 
-}
+  }
 
-finally {
+  finally {
     await db.close();
-  
+
     viewAllMembers()
-}
+  }
 
 }
 
@@ -323,4 +326,189 @@ export async function clearUserTable() {
   } finally {
     await db.close()
   }
+}
+
+export async function createProductTable() {
+  const db = await open({
+    filename: path.join('database.db'),
+    driver: sqlite3.Database
+  })
+  await db.exec("DROP TABLE IF EXISTS products");
+  await db.exec(`
+            CREATE TABLE  products (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  sku TEXT UNIQUE NOT NULL,
+                  name TEXT NOT NULL,
+                  description TEXT,
+                  category_id INTEGER,
+                  brand TEXT,
+                  price REAL NOT NULL,
+                  discount_price REAL,
+                  currency TEXT DEFAULT 'USD',
+                  stock INTEGER DEFAULT 0,
+                  status TEXT DEFAULT 'active',
+                  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                  FOREIGN KEY (category_id) REFERENCES categories(id) 
+            )
+
+      `)
+
+  await db.close()
+
+}
+
+export async function createCategoriesTable() {
+  const db = await open({
+    filename: path.join('database.db'),
+    driver: sqlite3.Database
+  })
+  await db.exec("DROP TABLE IF EXISTS categories");
+  await db.exec(`
+            CREATE TABLE  categories (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  name TEXT NOT NULL,
+                  parent_id INTEGER,
+                  FOREIGN KEY (parent_id) REFERENCES categories(id)
+            )
+      
+      `)
+
+  await db.close()
+
+}
+
+export async function createProductMediaTable() {
+  const db = await open({
+    filename: path.join('database.db'),
+    driver: sqlite3.Database
+  })
+  await db.exec("DROP TABLE IF EXISTS product_media");
+  await db.exec(`
+            CREATE TABLE  product_media (
+                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                product_id INTEGER,
+                media_type TEXT CHECK(media_type IN ('image', 'video')),
+                url TEXT NOT NULL,
+                FOREIGN KEY (product_id) REFERENCES products(id)
+            )
+      
+      `)
+
+  await db.close()
+
+}
+
+
+export async function createProductAttributeTable() {
+  const db = await open({
+    filename: path.join('database.db'),
+    driver: sqlite3.Database
+  })
+  await db.exec("DROP TABLE IF EXISTS product_attributes");
+  await db.exec(`
+            CREATE TABLE  product_attributes (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                product_id INTEGER,
+                attribute_name TEXT NOT NULL,
+                attribute_value TEXT,
+                FOREIGN KEY (product_id) REFERENCES products(id)
+            )
+      
+      `)
+
+  await db.close()
+
+}
+
+export async function createOrdersTable() {
+  const db = await open({
+    filename: path.join('database.db'),
+    driver: sqlite3.Database
+  })
+  await db.exec("DROP TABLE IF EXISTS orders");
+  await db.exec(`
+            CREATE TABLE orders (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
+                total_amount REAL,
+                status TEXT DEFAULT 'pending',
+                shipping_address TEXT,
+                payment_method TEXT,
+                created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users(id)
+);
+      `)
+
+  await db.close()
+
+}
+
+
+export async function createOrderItemsTable() {
+  const db = await open({
+    filename: path.join('database.db'),
+    driver: sqlite3.Database
+  })
+  await db.exec("DROP TABLE IF EXISTS order_items");
+  await db.exec(`
+            CREATE TABLE order_items (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  order_id INTEGER,
+                  product_id INTEGER,
+                  quantity INTEGER NOT NULL,
+                  price REAL NOT NULL,
+                  FOREIGN KEY (order_id) REFERENCES orders(id),
+                  FOREIGN KEY (product_id) REFERENCES products(id)
+);
+      `)
+
+  await db.close()
+
+}
+
+
+export async function createReviewsTable() {
+  const db = await open({
+    filename: path.join('database.db'),
+    driver: sqlite3.Database
+  })
+  await db.exec("DROP TABLE IF EXISTS reviews");
+  await db.exec(`
+           CREATE TABLE reviews (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  product_id INTEGER,
+                  user_id INTEGER,
+                  rating INTEGER CHECK(rating >= 1 AND rating <= 5),
+                  comment TEXT,
+                  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                  FOREIGN KEY (product_id) REFERENCES products(id),
+                  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+      `)
+
+  await db.close()
+
+}
+
+
+export async function createWishListTable() {
+  const db = await open({
+    filename: path.join('database.db'),
+    driver: sqlite3.Database
+  })
+  await db.exec("DROP TABLE IF EXISTS reviews");
+  await db.exec(`
+          CREATE TABLE wishlist (
+                  id INTEGER PRIMARY KEY AUTOINCREMENT,
+                  user_id INTEGER,
+                  product_id INTEGER,
+                  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                  FOREIGN KEY (user_id) REFERENCES users(id),
+                  FOREIGN KEY (product_id) REFERENCES products(id)
+);
+      `)
+
+  await db.close()
+
 }
--- database/login.js
+++ database/login.js
@@ -2,12 +2,27 @@ export const members = [
     {
         username: 'admin',
         password: 'admin123',
-        role: 'admin'
+        role: 'admin',
+        phone: '1234567890',
+        address: '123 Admin St, Admin City',
+        created_at: new Date().toISOString()
     },
     {
         username: 'ali',
         password: 'ali',
-        role: 'user'
+        role: 'user',
+        phone: '1234567890',
+        address: '123 Admin St, Admin City',
+        created_at: new Date().toISOString()
+    },
+    {
+        username: 'dev',
+        password: 'dev',
+        role: 'user',
+        phone: '1234567890',
+        address: '123 Admin St, Admin City',
+        created_at: new Date().toISOString()
     }
+
 
 ]
